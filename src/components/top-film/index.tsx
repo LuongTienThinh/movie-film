c076214d7d88363139c8884171b8f217b28a5ddf
Tylor Luong <tylor@digityze.asia>
2024-12-18 09:46:34 +0700

    update font montserrat

--- src/components/top-film/index.tsx
+++ src/components/top-film/index.tsx
@@ -28,9 +28,9 @@ const TopFilm = ({ name, poster_url, thumbnail_url, slug, year, rank }: ITopFilm
           <img className='h-p12 w-p12' src={poster_url} alt='' />
         </div>
         <div className='relative z-10 flex h-full w-full items-center gap-4'>
-          <div className='w-1/5 text-center text-8xl font-extrabold'>{rank}</div>
+          <div className='w-1/5 text-center text-8xl font-bold'>{rank}</div>
           <div className='flex h-full w-4/5 flex-col justify-between'>
-            <div className='name text-lg font-extrabold uppercase'>{name}</div>
+            <div className='name text-lg font-bold uppercase'>{name}</div>
             <div className='year self-end'>{year}</div>
           </div>
         </div>
